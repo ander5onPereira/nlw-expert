243e22ecdc54fe5e7078ec90e147da461db5a6e6
Anderson Pereira <anderson.carro.95@gmail.com>
2024-02-08 01:59:10 -0300

    feature: funcionalidade de busca cadastro, delete de notas, ajuste de layout e responsividade

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,10 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "no-shadow": "off",
+    "react-hooks/exhaustive-deps": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,79 @@
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/newNoteCard";
 import { NoteCard } from "./components/noteCard";
 
+interface NoteInterface {
+  id: string;
+  date: Date;
+  content: string;
+}
+
 export function App() {
+  const [search, setSearch] = useState("");
+  const [notes, setNotes] = useState<Array<NoteInterface>>(() => {
+    const notesOnStorage = localStorage.getItem("notes-nlw-expert");
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage);
+    }
+    return [];
+  });
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    } as NoteInterface;
+    const notesArray = [newNote, ...notes];
+
+    setNotes(notesArray);
+
+    localStorage.setItem("notes-nlw-expert", JSON.stringify(notesArray));
+  }
+
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter((note) => {
+      return note.id !== id;
+    });
+    setNotes(notesArray);
+
+    localStorage.setItem("notes-nlw-expert", JSON.stringify(notesArray));
+  }
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+    setSearch(query);
+  }
+
+  const filteredNotes =
+    search !== ""
+      ? notes.filter((note) =>
+          note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )
+      : notes;
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="nlw expert" />
       <form className="w-full">
         <input
           type="text"
           placeholder="Busque em suas notas"
-          name=""
-          id=""
+          name="search"
+          id="search"
+          onChange={handleSearch}
+          value={search}
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
         />
       </form>
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
-        <NewNoteCard />
-
-        <NoteCard />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {filteredNotes.map((note) => {
+          return (
+            <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
+          );
+        })}
       </div>
     </div>
   );
